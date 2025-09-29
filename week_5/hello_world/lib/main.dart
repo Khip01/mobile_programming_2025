e9dd1d3aaf4cc16dc06355ad6b6342ce6898bd40
Khip01 <aakhifathallah24@gmail.com>
2025-09-29 20:25:46 +0700

    feat: added Date and Time Pickers

--- week_5/hello_world/lib/main.dart
+++ week_5/hello_world/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 // import 'package:hello_world/basic_widgets/image_widget.dart';
 // import 'package:hello_world/basic_widgets/text_widget.dart';
 
@@ -25,17 +26,22 @@ class MyApp extends StatelessWidget {
     //   ),
     // );
     // ----- Test Input dan Selection Widget -----
+    // return MaterialApp(
+    //   home: Scaffold(
+    //     appBar: AppBar(title: const Text("Contoh TextField")),
+    //     body: const TextField(
+    //       obscureText: false,
+    //       decoration: InputDecoration(
+    //         border: OutlineInputBorder(),
+    //         labelText: 'Nama',
+    //       ),
+    //     ),
+    //   ),
+    // );
+    // ----- Test Date and Time Pickers -----
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text("Contoh TextField")),
-        body: const TextField(
-          obscureText: false,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'Nama',
-          ),
-        ),
-      ),
+      title: 'Flutter Date and Time Pickers',
+      home: const DateAndTimePickers(title: 'Flutter Date and Time Pickers'),
     );
   }
 }
@@ -71,9 +77,7 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             // const MyTextWidget(),
             // const MyImageWidget(),
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            const Text('You have pushed the button this many times:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
@@ -81,11 +85,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          height: 50.0,
-        ),
-      ),
+      bottomNavigationBar: BottomAppBar(child: Container(height: 50.0)),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment Counter',
@@ -126,9 +126,7 @@ showAlertDialog(BuildContext context) {
   AlertDialog alert = AlertDialog(
     title: const Text("My title"),
     content: const Text("This is my message."),
-    actions: [
-      okButton,
-    ],
+    actions: [okButton],
   );
 
   // show the dialog
@@ -138,4 +136,60 @@ showAlertDialog(BuildContext context) {
       return alert;
     },
   );
+}
+
+class DateAndTimePickers extends StatefulWidget {
+  const DateAndTimePickers({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _DateAndTimePickersState createState() => _DateAndTimePickersState();
+}
+
+class _DateAndTimePickersState extends State<DateAndTimePickers> {
+  // Variable/State untuk mengambil tanggal
+  DateTime selectedDate = DateTime.now();
+
+  //  Initial SelectDate FLutter
+  Future<void> _selectDate(BuildContext context) async {
+    // Initial DateTime FIinal Picked
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2015, 8),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text("${selectedDate.toLocal()}".split(' ')[0]),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () => {
+                _selectDate(context),
+                // ignore: avoid_print
+                print(
+                  selectedDate.day + selectedDate.month + selectedDate.year,
+                ),
+              },
+              child: const Text('Pilih Tanggal'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
