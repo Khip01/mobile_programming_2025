d56de8b2c000ed4b464adad59f0d2f8a73ccafa5
Khip01 <aakhifathallah24@gmail.com>
2025-09-30 07:36:50 +0700

    feat: updated README.md
    ~ tugas praktikum

--- week_5/hello_world/README.md
+++ week_5/hello_world/README.md
@@ -4,13 +4,54 @@ A new Flutter project.
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+### Praktikum 1: Membuat Project Flutter Baru
+Saya membuat project baru di Android Studio.
+[![create_new_proj](readme_assets/create_new_proj.png)]()
 
-A few resources to get you started if this is your first Flutter project:
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+### Praktikum 2: Menghubungkan Perangkat Android atau Emulator
+Saya menggunakan perangkat fisik android untuk menjalankan project flutter saya. Saya tinggal melakukan koneksi ke wifi yang saya dengan device fisik saya, lalu melakukan beberapa perintah berikut untuk mengkoneksikan ke perangkan fisik saya,
+[![create_new_proj](readme_assets/connect_to_physical_device.png)]()
+[![create_new_proj](readme_assets/connect_to_physical_device_connected.png)]()
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+### Praktikum 3: Membuat Repository GitHub dan Laporan Praktikum
+Setelah itu melakukan push project ke github, dan saya sudah melakukannya seperti berikut,
+[![create_new_proj](readme_assets/project_push_repo.png)]()
+
+### Praktikum 4: Menerapkan Widget Dasar
+#### Text Widget
+Saya sudah menambahkan widget baru untuk menampilkan Text sesuai dengan keinginan saya,
+[![create_new_proj](readme_assets/text_widget.png)]()
+
+#### Image Widget
+Lalu untuk image widget saya juga sudah melakukan:
+- meletakkan logo polinema di assets
+- mengubah pubspect.yaml untuk image path logo dari assets
+- menampilkan di layar dengan widget image
+
+[![create_new_proj](readme_assets/image_widget.png)]()
+
+### Praktikum 5
+#### Scaffold Widget
+Disini saya membuat file baru bernama `scaffold_widget.dart`,
+[![create_new_proj](readme_assets/scaffold_widget_dir.png)]()
+Lalu tampilannya seperti berikut,
+[![create_new_proj](readme_assets/scaffold_widget.png)]()
+
+#### Dialog Widget
+Disini saya membuat file baru bernama `dialog_widget.dart`,
+[![create_new_proj](readme_assets/dialog_widget_dir.png)]()
+Lalu tampilannya seperti berikut,
+[![create_new_proj](readme_assets/dialog_widget.png)]()
+
+#### Input dan Selection Widget
+Disini saya membuat file baru bernama `input_widget.dart`,
+[![create_new_proj](readme_assets/input_widget_dir.png)]()
+Lalu tampilannya seperti berikut,
+[![create_new_proj](readme_assets/input_widget.png)]()
+
+#### Date and Time Pickers
+Disini saya membuat file baru bernama `date_and_time_widget.dart`,
+[![create_new_proj](readme_assets/date_and_time_widget_dir.png)]()
+Lalu tampilannya seperti berikut,
+[![create_new_proj](readme_assets/date_and_time_widget.png)]()
--- week_5/hello_world/lib/main.dart
+++ week_5/hello_world/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/basic_widgets/date_and_time_widget.dart';
+import 'package:hello_world/basic_widgets/dialog_widget.dart';
+import 'package:hello_world/basic_widgets/input_widget.dart';
+import 'package:hello_world/basic_widgets/scaffold_widget.dart';
 
-// import 'package:hello_world/basic_widgets/image_widget.dart';
-// import 'package:hello_world/basic_widgets/text_widget.dart';
+import 'package:hello_world/basic_widgets/image_widget.dart';
+import 'package:hello_world/basic_widgets/text_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,25 +23,14 @@ class MyApp extends StatelessWidget {
     //   ),
     //   home: const MyHomePage(title: 'Flutter Demo Home Page'),
     // );
-    // ----- Test MyLayout Widget -----
+    // ----- Test MyLayout Widget (Dialog) -----
     // return MaterialApp(
     //   home: Scaffold(
     //     body: MyLayout(),
     //   ),
     // );
     // ----- Test Input dan Selection Widget -----
-    // return MaterialApp(
-    //   home: Scaffold(
-    //     appBar: AppBar(title: const Text("Contoh TextField")),
-    //     body: const TextField(
-    //       obscureText: false,
-    //       decoration: InputDecoration(
-    //         border: OutlineInputBorder(),
-    //         labelText: 'Nama',
-    //       ),
-    //     ),
-    //   ),
-    // );
+    // return InputWidget();
     // ----- Test Date and Time Pickers -----
     return MaterialApp(
       title: 'Flutter Date and Time Pickers',
@@ -46,150 +39,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // const MyTextWidget(),
-            // const MyImageWidget(),
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(child: Container(height: 50.0)),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment Counter',
-        child: const Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-    );
-  }
-}
-
-class MyLayout extends StatelessWidget {
-  const MyLayout({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ElevatedButton(
-        child: const Text('Show alert'),
-        onPressed: () {
-          showAlertDialog(context);
-        },
-      ),
-    );
-  }
-}
-
-showAlertDialog(BuildContext context) {
-  // set up the button
-  Widget okButton = TextButton(
-    child: const Text("OK"),
-    onPressed: () {
-      Navigator.pop(context);
-    },
-  );
-
-  // set up the AlertDialog
-  AlertDialog alert = AlertDialog(
-    title: const Text("My title"),
-    content: const Text("This is my message."),
-    actions: [okButton],
-  );
-
-  // show the dialog
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return alert;
-    },
-  );
-}
-
-class DateAndTimePickers extends StatefulWidget {
-  const DateAndTimePickers({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _DateAndTimePickersState createState() => _DateAndTimePickersState();
-}
-
-class _DateAndTimePickersState extends State<DateAndTimePickers> {
-  // Variable/State untuk mengambil tanggal
-  DateTime selectedDate = DateTime.now();
-
-  //  Initial SelectDate FLutter
-  Future<void> _selectDate(BuildContext context) async {
-    // Initial DateTime FIinal Picked
-    final DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: selectedDate,
-      firstDate: DateTime(2015, 8),
-      lastDate: DateTime(2101),
-    );
-    if (picked != null && picked != selectedDate) {
-      setState(() {
-        selectedDate = picked;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text("${selectedDate.toLocal()}".split(' ')[0]),
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () => {
-                _selectDate(context),
-                // ignore: avoid_print
-                print(
-                  selectedDate.day + selectedDate.month + selectedDate.year,
-                ),
-              },
-              child: const Text('Pilih Tanggal'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
